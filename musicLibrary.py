443bdee5ba4362d5503c1c80abe34b43c98138b7
Gauravi Mungia <gauravi4790.se24@chitkara.edu.in>
2026-04-18 12:00:41 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music={
+    "cook":"https://youtu.be/7tfHDdI5ATY?si=8icIXIbOSfqyA758" ,
+    "simple":"https://youtu.be/YlqSZLjj7as?si=lQQAcdmC0f8NR7tX",
+    "dil" : "https://youtu.be/or8LyI2eneg?si=db1papk0GZ6rc0IZ",
+    "song" : "https://youtu.be/W0DM5lcj6mw?si=gLoP1z4kq3sRPjav"
+}
